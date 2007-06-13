73e4a01c477d4abe2099f9833b885b5f2fa7f486
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2007-06-13 02:56:23 +0000

    for Trac#103 Icon on another windows is changed.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@111 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ide/ObjectList/ObjectList/SearchInstance.Designer.cs
+++ ide/ObjectList/ObjectList/SearchInstance.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchInstance));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.searchButton = new System.Windows.Forms.Button();
@@ -131,6 +132,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 73);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SearchInstance";
             this.Text = "SearchInstance";
             this.tableLayoutPanel1.ResumeLayout(false);
