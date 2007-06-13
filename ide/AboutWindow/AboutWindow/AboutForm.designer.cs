61e92ae6905392de3b011bbf1a45690b81105e05
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2007-06-13 02:59:08 +0000

    for Trac#103 Icon on another windows is changed.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@113 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ide/AboutWindow/AboutWindow/AboutForm.designer.cs
+++ ide/AboutWindow/AboutWindow/AboutForm.designer.cs
@@ -189,6 +189,7 @@ namespace EcellLib.AboutWindow
             this.ClientSize = new System.Drawing.Size(388, 182);
             this.Controls.Add(this.tableLayoutPanel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
