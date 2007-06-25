609a3e5a3b1f12a7b2c9bd571a83d3d98fb8402e
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2007-06-25 06:09:43 +0000

    for Trac#105.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@194 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ide/AboutWindow/AboutWindow/AboutForm.designer.cs
+++ ide/AboutWindow/AboutWindow/AboutForm.designer.cs
@@ -47,42 +47,25 @@ namespace EcellLib.AboutWindow
             // 
             // copyLabel
             // 
-            this.copyLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.copyLabel.AutoSize = true;
+            resources.ApplyResources(this.copyLabel, "copyLabel");
             this.tableLayoutPanel2.SetColumnSpan(this.copyLabel, 2);
-            this.copyLabel.Location = new System.Drawing.Point(142, 146);
             this.copyLabel.Name = "copyLabel";
-            this.copyLabel.Size = new System.Drawing.Size(220, 12);
-            this.copyLabel.TabIndex = 1;
-            this.copyLabel.Text = "Copyright (c) 2005 - 2006 Keio University";
             // 
             // ecellLink
             // 
-            this.ecellLink.AutoSize = true;
-            this.ecellLink.Location = new System.Drawing.Point(120, 83);
+            resources.ApplyResources(this.ecellLink, "ecellLink");
             this.ecellLink.Name = "ecellLink";
-            this.ecellLink.Size = new System.Drawing.Size(111, 12);
-            this.ecellLink.TabIndex = 1;
             this.ecellLink.TabStop = true;
-            this.ecellLink.Text = "E-Cell Project Home";
             // 
             // manualLink
             // 
-            this.manualLink.AutoSize = true;
-            this.manualLink.Location = new System.Drawing.Point(120, 111);
+            resources.ApplyResources(this.manualLink, "manualLink");
             this.manualLink.Name = "manualLink";
-            this.manualLink.Size = new System.Drawing.Size(81, 12);
-            this.manualLink.TabIndex = 2;
             this.manualLink.TabStop = true;
-            this.manualLink.Text = "Online Manual ";
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 4;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 7F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 110F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 7F));
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
             this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 1, 1);
             this.tableLayoutPanel2.Controls.Add(this.ecellLink, 2, 5);
             this.tableLayoutPanel2.Controls.Add(this.label3, 2, 3);
@@ -91,110 +74,61 @@ namespace EcellLib.AboutWindow
             this.tableLayoutPanel2.Controls.Add(this.closeButton, 1, 7);
             this.tableLayoutPanel2.Controls.Add(this.manualLink, 2, 6);
             this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 2, 1);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 9;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 7F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 7F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 17F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 7F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(388, 182);
-            this.tableLayoutPanel2.TabIndex = 6;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
             this.tableLayoutPanel2.SetRowSpan(this.pictureBox1, 6);
-            this.pictureBox1.Size = new System.Drawing.Size(100, 94);
-            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // label3
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(120, 41);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(255, 12);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "E-Cell Platform is distributed under the terms of ";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // gnuLink
             // 
-            this.gnuLink.AutoSize = true;
-            this.gnuLink.Location = new System.Drawing.Point(120, 56);
+            resources.ApplyResources(this.gnuLink, "gnuLink");
             this.gnuLink.Name = "gnuLink";
-            this.gnuLink.Size = new System.Drawing.Size(183, 12);
-            this.gnuLink.TabIndex = 0;
             this.gnuLink.TabStop = true;
-            this.gnuLink.Text = "GNU General Public License (GPL)";
             // 
             // closeButton
             // 
-            this.closeButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.closeButton.Location = new System.Drawing.Point(10, 140);
+            resources.ApplyResources(this.closeButton, "closeButton");
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(80, 23);
-            this.closeButton.TabIndex = 6;
-            this.closeButton.Text = "OK";
             this.closeButton.UseVisualStyleBackColor = true;
             this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.43411F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.56589F));
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.versionLabel, 1, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(120, 10);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(258, 19);
-            this.tableLayoutPanel1.TabIndex = 7;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 0);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 12);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "E-Cell Platform";
             // 
             // versionLabel
             // 
-            this.versionLabel.AutoSize = true;
-            this.versionLabel.Location = new System.Drawing.Point(97, 0);
+            resources.ApplyResources(this.versionLabel, "versionLabel");
             this.versionLabel.Name = "versionLabel";
-            this.versionLabel.Size = new System.Drawing.Size(42, 12);
-            this.versionLabel.TabIndex = 1;
-            this.versionLabel.Text = "version";
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 182);
             this.Controls.Add(this.tableLayoutPanel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.Text = "About E-Cell Platform";
             this.Load += new System.EventHandler(this.About_Load);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
