8d50c36799d8a4e57fdd947af53099942e8f002f
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2007-11-29 10:07:26 +0000

    Enhancement for trac#39, Refactoring
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@697 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/PathwayWindow/PathwayWindow/UIComponent/InputBox.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace EcellLib.PathwayWindow.UIComponent
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputText.Location = new System.Drawing.Point(12, 88);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(268, 19);
+            this.inputText.TabIndex = 0;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(205, 12);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(205, 50);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // message
+            // 
+            this.message.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.message.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.message.Location = new System.Drawing.Point(14, 16);
+            this.message.Multiline = true;
+            this.message.Name = "message";
+            this.message.ReadOnly = true;
+            this.message.Size = new System.Drawing.Size(185, 57);
+            this.message.TabIndex = 4;
+            this.message.TabStop = false;
+            // 
+            // InputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 117);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.inputText);
+            this.Name = "InputBox";
+            this.Text = "InputBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox message;
+    }
+}
---
+++ ide/PathwayWindow/PathwayWindow/UIComponent/InputBox.cs
@@ -0,0 +1,118 @@
+﻿//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//        This file is part of E-Cell Environment Application package
+//
+//                Copyright (C) 1996-2006 Keio University
+//
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//
+// E-Cell is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// E-Cell is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with E-Cell -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+//END_HEADER
+//
+// written by by Chihiro Okada <c_okada@cbo.mss.co.jp>,
+// MITSUBISHI SPACE SOFTWARE CO.,LTD.
+//
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EcellLib.PathwayWindow.UIComponent
+{
+    public partial class InputBox : Form
+    {
+        #region Accessor
+        /// <summary>
+        ///  get/set message text.
+        /// </summary>
+        public string Message
+        {
+            get { return this.message.Text; }
+            set { this.message.Text = value; }
+        }
+        /// <summary>
+        ///  get/set message text.
+        /// </summary>
+        public string Input
+        {
+            get { return this.inputText.Text; }
+            set { this.inputText.Text = value; }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public InputBox(string message)
+        {
+            InitializeComponent();
+            this.Message = message;
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="title"></param>
+        public InputBox(string message, string title)
+        {
+            InitializeComponent();
+            this.Message = message;
+            this.Text = title;
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="title"></param>
+        /// <param name="defaultAns"></param>
+        public InputBox(string message, string title, string defaultAns)
+        {
+            InitializeComponent();
+            this.Message = message;
+            this.Text = title;
+            this.Input = defaultAns;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Show InputBoxDialog.
+        /// </summary>
+        /// <returns></returns>
+        public new DialogResult Show()
+        {
+            this.ShowDialog();
+            return this.DialogResult;
+        }
+        #endregion
+    }
+}
