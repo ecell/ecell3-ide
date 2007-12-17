d3427853f5479451a36910b52d045c33dbb6584b
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2007-12-17 13:48:50 +0000

    refactoring of PathwayWindow
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@739 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/PathwayWindow/PathwayWindow/Handler/ObjectDragHandler.cs
@@ -0,0 +1,54 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//        This file is part of E-Cell Environment Application package
+//
+//                Copyright (C) 1996-2006 Keio University
+//
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//
+// E-Cell is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// E-Cell is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with E-Cell -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+//END_HEADER
+//
+// written by Motokazu Ishikawa <m.ishikawa@cbo.mss.co.jp>,
+// MITSUBISHI SPACE SOFTWARE CO.,LTD.
+//
+// modified by Chihiro Okada <c_okada@cbo.mss.co.jp>,
+// MITSUBISHI SPACE SOFTWARE CO.,LTD.
+
+using UMD.HCIL.Piccolo.Event;
+
+namespace EcellLib.PathwayWindow
+{
+    /// <summary>
+    /// Drag handler for resize handles of system.
+    /// </summary>
+    public class ObjectDragHandler : PDragEventHandler
+    {
+        /// <summary>
+        /// Called when dragging of a resize handle started.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void OnStartDrag(object sender, PInputEventArgs e)
+        {
+            base.OnStartDrag(sender, e);
+            e.Handled = true;
+            e.PickedNode.MoveToFront();
+        }
+    }
+}
