dafcfdaf116399b5f50d515641cdddc43d5c9a7a
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2007-12-24 17:13:33 +0000

    refactoring of PathwayWindow Implementation of ToolBox
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@760 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/PathwayWindow/PathwayWindow/Handler/ToolBoxEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EcellLib.PathwayWindow.Handler
+{
+    class ToolBoxEventHandler : PPathwayInputEventHandler
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="control"></param>
+        public ToolBoxEventHandler(PathwayControl control)
+        {
+            base.m_con = control;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnMouseDown(object sender, UMD.HCIL.Piccolo.Event.PInputEventArgs e)
+        {
+            base.OnMouseDown(sender, e);
+            MessageBox.Show("I'm sorry to tell you that \nthis function is under construction.");
+        }
+    }
+}
--- ide/PathwayWindow/PathwayWindow/UIComponent/ToolBox.cs
+++ ide/PathwayWindow/PathwayWindow/UIComponent/ToolBox.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using EcellLib.PathwayWindow.Nodes;
+using EcellLib.PathwayWindow.Handler;
 
 namespace EcellLib.PathwayWindow.UIComponent
 {
@@ -21,20 +22,20 @@ namespace EcellLib.PathwayWindow.UIComponent
 
         private void SetToolBoxItems()
         {
-            float left = 30;
-            float top = 60;
             PPathwayLayer layer1 = new PPathwayLayer();
             pCanvas1.Root.AddChild(layer1);
             pCanvas1.Camera.AddLayer(layer1);
             pCanvas1.Camera.ScaleViewBy(0.7f);
             pCanvas1.RemoveInputEventListener(pCanvas1.PanEventHandler);
             pCanvas1.RemoveInputEventListener(pCanvas1.ZoomEventHandler);
+            pCanvas1.AddInputEventListener(new ToolBoxEventHandler(m_con));
+            RectangleF bounds = pCanvas1.Camera.ViewBounds;
+            PointF center = new PointF(bounds.X + bounds.Width / 2f, bounds.Y + bounds.Height / 2f);
             PPathwayObject obj1 = m_con.ComponentManager.DefaultSystemSetting.CreateTemplate();
             obj1.Pickable = false;
-            obj1.X = left;
-            obj1.Y = Top;
             obj1.Width = PPathwaySystem.MIN_X_LENGTH;
             obj1.Height = PPathwaySystem.MIN_Y_LENGTH;
+            obj1.CenterPointF = center;
             layer1.AddChild(obj1);
             
             PPathwayLayer layer2 = new PPathwayLayer();
@@ -43,10 +44,10 @@ namespace EcellLib.PathwayWindow.UIComponent
             pCanvas2.Camera.ScaleViewBy(0.7f);
             pCanvas2.RemoveInputEventListener(pCanvas2.PanEventHandler);
             pCanvas2.RemoveInputEventListener(pCanvas2.ZoomEventHandler);
+            pCanvas2.AddInputEventListener(new ToolBoxEventHandler(m_con));
             PPathwayObject obj2 = m_con.ComponentManager.DefaultVariableSetting.CreateTemplate();
             obj2.Pickable = false;
-            obj2.X = left;
-            obj2.Y = Top;
+            obj2.CenterPointF = center;
             layer2.AddChild(obj2);
 
             PPathwayLayer layer3 = new PPathwayLayer();
@@ -55,10 +56,10 @@ namespace EcellLib.PathwayWindow.UIComponent
             pCanvas3.Camera.ScaleViewBy(0.7f);
             pCanvas3.RemoveInputEventListener(pCanvas3.PanEventHandler);
             pCanvas3.RemoveInputEventListener(pCanvas3.ZoomEventHandler);
+            pCanvas3.AddInputEventListener(new ToolBoxEventHandler(m_con));
             PPathwayObject obj3 = m_con.ComponentManager.DefaultProcessSetting.CreateTemplate();
             obj3.Pickable = false;
-            obj3.X = left;
-            obj3.Y = Top;
+            obj3.CenterPointF = center;
             layer3.AddChild(obj3);
         }
     }
