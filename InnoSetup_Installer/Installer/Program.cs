a37ffd93b07cdc77286a34612a9e4f0341d071d1
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-05 14:54:34 +0000

    create installer template.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@780 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ InnoSetup_Installer/Installer/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Cell_IDE_Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
