77d1955e39d07b3b8be702b7836218c9e96d9ade
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-05 17:42:41 +0000

    refactoring
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@786 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- InnoSetup_Installer/Installer/Program.cs
+++ InnoSetup_Installer/Installer/Program.cs
@@ -1,13 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace E_Cell_IDE_Installer
 {
     class Program
     {
+        [System.Runtime.InteropServices.DllImport("shell32.dll")]
+        public static extern int FindExecutable(
+            string lpFile, string lpDirectory,
+            System.Text.StringBuilder lpResult);
+
+
         static void Main(string[] args)
         {
+            //関連付けられた実行ファイルを取得するファイル名
+            string fileName =  args[0];
+            //結果を受け取るためのStringBuilderオブジェクト
+            System.Text.StringBuilder exePath =
+                new System.Text.StringBuilder(255);
+            //fileNameに関連付けられた実行ファイルのパスを取得する
+            if (FindExecutable(fileName, null, exePath) > 32)
+            {
+                //成功した時は、exePathの内容を表示する
+                Console.WriteLine(exePath.ToString());
+                Process proc = new Process();
+                proc.StartInfo.FileName = exePath.ToString();
+                proc.StartInfo.Arguments = fileName;
+                proc.Start();
+                proc.WaitForExit();
+
+
+            }
+            else
+            {
+                Console.WriteLine("失敗しました。");
+            }
         }
     }
 }
