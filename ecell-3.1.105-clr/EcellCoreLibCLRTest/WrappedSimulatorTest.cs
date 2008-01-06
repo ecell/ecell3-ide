88ed3009f87c77ddb6d423c810f6642cec42e690
moriyoshi <moriyoshi@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-06 08:53:16 +0000

    テストプロジェクトの追加
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@790 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ecell-3.1.105-clr/EcellCoreLibCLRTest/WrappedSimulatorTest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EcellCoreLib;
+using NUnit.Framework;
+
+namespace EcellCoreLibCLRTest
+{
+    [TestFixture]
+    public class WrappedSimulatorTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
