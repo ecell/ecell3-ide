dbc5847c115c2c6feb1bfe2410a0cbc436a620d9
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-08 08:44:01 +0000

    refactoring of EcellLib.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@804 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/EcellLib/EcellLib/test/DataManagerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EcellCoreLib;
+using NUnit.Framework;
+using EcellLib;
+
+namespace test
+{
+    [TestFixture]
+    public class DataManagerTest
+    {
+        private DataManager m_dManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_dManager = DataManager.GetDataManager();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            m_dManager.CreateProject("testProject", "comment", null, new List<string>());
+            m_dManager.SaveProject("testProject");
+        }
+    }
+}
