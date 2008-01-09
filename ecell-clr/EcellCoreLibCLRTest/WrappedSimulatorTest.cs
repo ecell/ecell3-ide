78ca4dc5bf26cf9d7932e6f7186fd0c7e9fcaecf
moriyoshi <moriyoshi@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-09 12:52:30 +0000

    テストを追加
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@820 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ecell-clr/EcellCoreLibCLRTest/WrappedSimulatorTest.cs
+++ ecell-clr/EcellCoreLibCLRTest/WrappedSimulatorTest.cs
@@ -140,11 +140,102 @@ namespace EcellCoreLibCLRTest
 
         [Test]
         public void TestCreateStepper()
-        {  
+        {
             s.CreateStepper("PassiveStepper", "testStepper");
             Assert.That(
                 s.GetStepperClassName("testStepper"),
                 Is.EqualTo("PassiveStepper"));
+        }
+
+        [Test]
+        public void TestEntityProperty()
+        {
+            string stepperID = GenerateRandomID();
+            string variableFullID = "Variable:/:_" + GenerateRandomID();
+
+            s.CreateStepper("PassiveStepper", stepperID);
+
+            {
+                s.SetEntityProperty(
+                    "System::/:StepperID",
+                    new WrappedPolymorph(stepperID));
+                WrappedPolymorph wp;
+                wp = s.GetEntityProperty("System::/:StepperID");
+                Assert.That(wp.IsString(), Is.True);
+                Assert.That(wp.CastToString(), Is.EqualTo(stepperID));
+                try
+                {
+                    wp = s.GetEntityProperty("System::/:NonExistentProperty");
+                    Assert.Fail();
+                }
+                catch (Exception e)
+                {
+                    Assert.That(e, Is.InstanceOfType(typeof(WrappedLibecsException)));
+                    Assert.That(e.Source, Is.EqualTo("NoSlot"));
+                }
+            }
+
+            s.CreateEntity("Variable", variableFullID);
+            {
+                double value = GenerateRandomDouble();
+                s.SetEntityProperty(
+                    variableFullID + ":Value",
+                    new WrappedPolymorph(value));
+                WrappedPolymorph wp;
+                wp = s.GetEntityProperty(variableFullID + ":Value");
+                Assert.That(wp.IsDouble(), Is.True);
+                Assert.That(wp.CastToDouble(), Is.EqualTo(value));
+                try
+                {
+                    wp = s.GetEntityProperty("INVALID:::::");
+                    Assert.Fail();
+                }
+                catch (Exception e)
+                {
+                    Assert.That(e, Is.InstanceOfType(typeof(WrappedLibecsException)));
+                    Assert.That(e.Source, Is.EqualTo("InvalidEntityType"));
+                }
+                try
+                {
+                    wp = s.GetEntityProperty("Variable:INVALID:INVALID:BAD");
+                    Assert.Fail();
+                }
+                catch (Exception e)
+                {
+                    Assert.That(e, Is.InstanceOfType(typeof(WrappedLibecsException)));
+                    Assert.That(e.Source, Is.EqualTo("BadSystemPath"));
+                }
+                try
+                {
+                    wp = s.GetEntityProperty("Variable:/:");
+                    Assert.Fail();
+                }
+                catch (Exception e)
+                {
+                    Assert.That(e, Is.InstanceOfType(typeof(WrappedLibecsException)));
+                    Assert.That(e.Source, Is.EqualTo("BadID"));
+                }
+                try
+                {
+                    wp = s.GetEntityProperty("Variable:/:NonExistent:Value");
+                    Assert.Fail();
+                }
+                catch (Exception e)
+                {
+                    Assert.That(e, Is.InstanceOfType(typeof(WrappedLibecsException)));
+                    Assert.That(e.Source, Is.EqualTo("NotFound"));
+                }
+                try
+                {
+                    wp = s.GetEntityProperty(variableFullID + ":NonExistentProperty");
+                    Assert.Fail();
+                }
+                catch (Exception e)
+                {
+                    Assert.That(e, Is.InstanceOfType(typeof(WrappedLibecsException)));
+                    Assert.That(e.Source, Is.EqualTo("NoSlot"));
+                }
+            }
         }
     }
 }
