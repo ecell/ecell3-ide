e4307e4ebfabcfe4e761c0b5feb838ca40f7a676
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-09 15:22:37 +0000

    refactoring of EcellLib.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@826 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/EcellLib/EcellLib/Test/TestPluginManager.cs
@@ -0,0 +1,382 @@
+namespace EcellLib
+{
+    using System;
+    using NUnit.Framework;
+
+
+    [TestFixture()]
+    public class TestPluginManager
+    {
+
+        private PluginManager _unitUnderTest;
+
+        [SetUp()]
+        public void SetUp()
+        {
+            _unitUnderTest = new PluginManager();
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+            _unitUnderTest = null;
+        }
+
+        [Test()]
+        public void TestConstructorPluginManager()
+        {
+            PluginManager testPluginManager = new PluginManager();
+            Assert.IsNotNull(testPluginManager, "Constructor of type, PluginManager failed to create instance.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestFocusDataChanged()
+        {
+            string modelID = null;
+            string key = null;
+            EcellLib.PluginBase pbase = null;
+            _unitUnderTest.FocusDataChanged(modelID, key, pbase);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestFocusClear()
+        {
+            _unitUnderTest.FocusClear();
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestSelectChanged()
+        {
+            string modelID = null;
+            string key = null;
+            string type = null;
+            _unitUnderTest.SelectChanged(modelID, key, type);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestAddSelect()
+        {
+            string modelID = null;
+            string key = null;
+            string type = null;
+            _unitUnderTest.AddSelect(modelID, key, type);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestRemoveSelect()
+        {
+            string modelID = null;
+            string key = null;
+            string type = null;
+            _unitUnderTest.RemoveSelect(modelID, key, type);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestResetSelect()
+        {
+            _unitUnderTest.ResetSelect();
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestLogData()
+        {
+            string modelID = null;
+            string key = null;
+            string type = null;
+            string propName = null;
+            System.Collections.Generic.List<EcellLib.LogData> data = null;
+            _unitUnderTest.LogData(modelID, key, type, propName, data);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestDataChanged()
+        {
+            string modelID = null;
+            string key = null;
+            string type = null;
+            EcellLib.EcellObject data = null;
+            _unitUnderTest.DataChanged(modelID, key, type, data);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestDataAdd()
+        {
+            System.Collections.Generic.List<EcellLib.EcellObject> data = null;
+            _unitUnderTest.DataAdd(data);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestDataDelete()
+        {
+            string modelID = null;
+            string key = null;
+            string type = null;
+            _unitUnderTest.DataDelete(modelID, key, type);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestParameterAdd()
+        {
+            string projectID = null;
+            string paramID = null;
+            _unitUnderTest.ParameterAdd(projectID, paramID);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestParameterDelete()
+        {
+            string projectID = null;
+            string paramID = null;
+            _unitUnderTest.ParameterDelete(projectID, paramID);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestWarnData()
+        {
+            string modelID = null;
+            string key = null;
+            string type = null;
+            string warntype = null;
+            _unitUnderTest.WarnData(modelID, key, type, warntype);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestLoadData()
+        {
+            string modelID = null;
+            _unitUnderTest.LoadData(modelID);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestAddPlugin()
+        {
+            EcellLib.PluginBase p = null;
+            _unitUnderTest.AddPlugin(p);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestShowSelectPlugin()
+        {
+            _unitUnderTest.ShowSelectPlugin();
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestPrint()
+        {
+            string pluginName = null;
+            _unitUnderTest.Print(pluginName);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestClear()
+        {
+            _unitUnderTest.Clear();
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestLoggerAdd()
+        {
+            string modelID = null;
+            string type = null;
+            string key = null;
+            string path = null;
+            _unitUnderTest.LoggerAdd(modelID, type, key, path);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestAdvancedTime()
+        {
+            double time = 0;
+            _unitUnderTest.AdvancedTime(time);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestMessage()
+        {
+            string type = null;
+            string message = null;
+            _unitUnderTest.Message(type, message);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestSaveModel()
+        {
+            string modelID = null;
+            string path = null;
+            _unitUnderTest.SaveModel(modelID, path);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestImageAdd()
+        {
+            string type = null;
+            int imageIndex = 0;
+            _unitUnderTest.ImageAdd(type, imageIndex);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestGetImageIndex()
+        {
+            string type = null;
+            int expectedInt32 = 0;
+            int resultInt32 = 0;
+            resultInt32 = _unitUnderTest.GetImageIndex(type);
+            Assert.AreEqual(expectedInt32, resultInt32, "GetImageIndex method returned unexpected result.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestLoadPlugin()
+        {
+            string path = null;
+            string className = null;
+            EcellLib.PluginBase expectedPluginBase = null;
+            EcellLib.PluginBase resultPluginBase = null;
+            resultPluginBase = _unitUnderTest.LoadPlugin(path, className);
+            Assert.AreEqual(expectedPluginBase, resultPluginBase, "LoadPlugin method returned unexpected result.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestUnloadPlugin()
+        {
+            EcellLib.PluginBase p = null;
+            _unitUnderTest.UnloadPlugin(p);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestCurrentToolBarMenu()
+        {
+            string expectedString = null;
+            string resultString = null;
+            resultString = _unitUnderTest.CurrentToolBarMenu();
+            Assert.AreEqual(expectedString, resultString, "CurrentToolBarMenu method returned unexpected result.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestChangeStatus()
+        {
+            EcellLib.ProjectStatus type = ProjectStatus.Uninitialized;
+            _unitUnderTest.ChangeStatus(type);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestChangeUndoStatus()
+        {
+            EcellLib.UndoStatus status = UndoStatus.NOTHING;
+            _unitUnderTest.ChangeUndoStatus(status);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestGetPlugin()
+        {
+            string name = null;
+            EcellLib.PluginBase expectedPluginBase = null;
+            EcellLib.PluginBase resultPluginBase = null;
+            resultPluginBase = _unitUnderTest.GetPlugin(name);
+            Assert.AreEqual(expectedPluginBase, resultPluginBase, "GetPlugin method returned unexpected result.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestGetLayoutPlugins()
+        {
+            System.Collections.Generic.List<EcellLib.ILayoutAlgorithm> expectedList = null;
+            System.Collections.Generic.List<EcellLib.ILayoutAlgorithm> resultList = null;
+            resultList = _unitUnderTest.GetLayoutPlugins();
+            Assert.AreEqual(expectedList, resultList, "GetLayoutPlugins method returned unexpected result.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestGetPluginVersionList()
+        {
+            System.Collections.Generic.Dictionary<System.String, System.String> expectedDictionary = null;
+            System.Collections.Generic.Dictionary<System.String, System.String> resultDictionary = null;
+            resultDictionary = _unitUnderTest.GetPluginVersionList();
+            Assert.AreEqual(expectedDictionary, resultDictionary, "GetPluginVersionList method returned unexpected result.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestGetPluginManager()
+        {
+            EcellLib.PluginManager expectedPluginManager = null;
+            EcellLib.PluginManager resultPluginManager = null;
+            resultPluginManager = PluginManager.GetPluginManager();
+            Assert.AreEqual(expectedPluginManager, resultPluginManager, "GetPluginManager method returned unexpected result.");
+            Assert.Fail("Create or modify test(s).");
+
+        }
+
+        [Test()]
+        public void TestSetPosition()
+        {
+            EcellLib.EcellObject data = null;
+            _unitUnderTest.SetPosition(data);
+            Assert.Fail("Create or modify test(s).");
+
+        }
+    }
+}
