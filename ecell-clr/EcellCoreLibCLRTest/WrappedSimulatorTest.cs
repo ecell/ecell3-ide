badb7c00ddcda4e072159688374df9fdde8d7057
moriyoshi <moriyoshi@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-09 07:25:07 +0000

    新しいステージングディレクトリ構成に対応。
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@817 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ecell-clr/EcellCoreLibCLRTest/WrappedSimulatorTest.cs
+++ ecell-clr/EcellCoreLibCLRTest/WrappedSimulatorTest.cs
@@ -3,25 +3,47 @@ using System.Collections.Generic;
 using System.Text;
 using EcellCoreLib;
 using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 
 namespace EcellCoreLibCLRTest
 {
     [TestFixture]
-    public class WrappedSimulatorTest
+    public class WrappedSimulatorTest: TestBase
     {
+        WrappedSimulator s;
+
         [SetUp]
         public void SetUp()
         {
+            s = new WrappedSimulator(
+                new string[] { GetDMDirectory() });
         }
 
         [TearDown]
         public void TearDown()
         {
+            s.Dispose();
         }
 
         [Test]
-        public void Test()
+        public void TestNone()
         {
+        }
+
+        [Test]
+        public void TestCreateEntity()
+        {
+            s.CreateEntity("System", "System::/");
+            Console.WriteLine(s.GetEntityList("System", ""));
+        }
+
+        [Test]
+        public void TestCreateStepper()
+        {  
+            s.CreateStepper("PassiveStepper", "testStepper");
+            Assert.That(
+                s.GetStepperClassName("testStepper"),
+                Is.EqualTo("testStepper"));
         }
     }
 }
