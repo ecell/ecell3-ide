a0188feb0822fb8908961291dbd910ee7d36f687
moriyoshi <moriyoshi@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-01-30 23:16:26 +0000

    * プラグインディレクトリ名を plugin から plugins に変更 * サンプルの DM の出力先を修正 * インストーラ作成に必要なアセンブリを REFPATH から検索してコピーするように修正 * E-Cell_IDE_Installer.exe をビルド後のステップで実行するよう修正 * E-Cell_IDE_Installer.exe が実行されたとき、正しいコマンドラインオプションで　ＩｎｎｏＳｅｔｕｐ が起動されるよう修正 * インストーラ出力先ディレクトリを現在のディレクトリに修正
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@874 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- InnoSetup_Installer/Program.cs
+++ InnoSetup_Installer/Program.cs
@@ -1,41 +1,67 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Text;
+using System.IO;
 
 namespace E_Cell_IDE_Installer
 {
     class Program
     {
-        [System.Runtime.InteropServices.DllImport("shell32.dll")]
-        public static extern int FindExecutable(
-            string lpFile, string lpDirectory,
-            System.Text.StringBuilder lpResult);
+        [DllImport("shell32.dll")]
+        private static extern uint ShellExecute(
+            IntPtr hwnd,
+            string lpVerb, string lpFile, string lpParameters,
+            string lpDirectory, int nShowCmd);
 
+        [DllImport("kernel32.dll")]
+        private static extern int FormatMessageW(
+            uint dwFlags, IntPtr lpSource,
+            uint dwMessageId, uint dwLanguageId,
+            out IntPtr lpMsgBuf, int nSize, IntPtr args);
+
+        [FlagsAttribute]
+        private enum FormatMessageFlags: uint
+        {
+            ALLOCATE_BUFFER = 0x00000100,
+            IGNORE_INSERTS = 0x00000200,
+            FROM_STRING = 0x00000400,
+            FROM_HMODULE = 0x00000800,
+            FROM_SYSTEM = 0x00001000,
+            ARGUMENT_ARRAY = 0x00002000,
+            MAX_WIDTH_MASK = 0x000000FF
+        };
+
+        [DllImport("kernel32.dll")]
+        private static extern void LocalFree(IntPtr lpPtr);
 
         static void Main(string[] args)
         {
             //関連付けられた実行ファイルを取得するファイル名
             string fileName =  args[0];
-            //結果を受け取るためのStringBuilderオブジェクト
-            System.Text.StringBuilder exePath =
-                new System.Text.StringBuilder(255);
             //fileNameに関連付けられた実行ファイルのパスを取得する
-            if (FindExecutable(fileName, null, exePath) > 32)
+            uint retval = ShellExecute(
+                IntPtr.Zero,
+                "Compile",
+                fileName, null,
+                Directory.GetCurrentDirectory(),
+                0);
+            if (retval <= 32)
             {
-                //成功した時は、exePathの内容を表示する
-                Console.WriteLine(exePath.ToString());
-                Process proc = new Process();
-                proc.StartInfo.FileName = exePath.ToString();
-                proc.StartInfo.Arguments = fileName;
-                proc.Start();
-                proc.WaitForExit();
-
-
-            }
-            else
-            {
-                Console.WriteLine("失敗しました。");
+                IntPtr lpMsg;
+                FormatMessageW(
+                    (uint)(FormatMessageFlags.ALLOCATE_BUFFER | FormatMessageFlags.FROM_SYSTEM),
+                    IntPtr.Zero, retval, (uint)0, out lpMsg, 0,
+                    IntPtr.Zero);
+                try
+                {
+                    Console.WriteLine("失敗しました (" + retval + ": " + Marshal.PtrToStringUni(lpMsg).TrimEnd() + ")");
+                }
+                finally
+                {
+                    LocalFree(lpMsg);
+                }
             }
         }
     }
