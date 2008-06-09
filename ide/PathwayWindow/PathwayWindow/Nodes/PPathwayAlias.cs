1c252e18e6e30e359bf340e6a1f7b9b30d7b90d0
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-06-09 05:08:53 +0000

    refactoring of PathwayWindow. リファクタリング
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@1311 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/PathwayWindow/PathwayWindow/Nodes/PPathwayAlias.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcellLib.PathwayWindow.Nodes
+{
+    class PPathwayAlias : PPathwayObject
+    {
+        PPathwayVariable m_variable = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="variable"></param>
+        public PPathwayAlias(PPathwayVariable variable)
+        {
+            m_variable = variable;
+            m_canvas = variable.Canvas;
+        }
+
+        public override PPathwayObject CreateNewObject()
+        {
+            return null;
+        }
+
+        public override void OnMouseDown(UMD.HCIL.Piccolo.Event.PInputEventArgs e)
+        {
+            m_variable.OnMouseDown(e);
+        }
+
+        internal void NotifyDataChanged()
+        {
+            m_ecellObj.X = this.X + this.OffsetX;
+            m_ecellObj.Y = this.Y + this.OffsetY;
+            m_variable.Canvas.Control.NotifyDataChanged(m_ecellObj.Key, m_variable.EcellObject, true, true);
+        }
+    }
+}
