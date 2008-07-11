730ff6eece0a74f77067f78cbe3d87f93b67dd8b
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2008-07-11 04:50:28 +0000

    refactoring.
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@1439 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ide/EntityListWindow/EntityListWindow/InputName.Designer.cs
+++ ide/EntityListWindow/EntityListWindow/InputName.Designer.cs
@@ -29,31 +29,11 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputName));
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.INNewButton = new System.Windows.Forms.Button();
             this.INCancelButton = new System.Windows.Forms.Button();
-            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.INTextBox = new System.Windows.Forms.TextBox();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
-            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            // 
-            // tableLayoutPanel2
-            // 
-            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
-            this.tableLayoutPanel2.Controls.Add(this.INNewButton, 1, 0);
-            this.tableLayoutPanel2.Controls.Add(this.INCancelButton, 3, 0);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             // 
             // INNewButton
             // 
@@ -69,13 +49,6 @@
             this.INCancelButton.Name = "INCancelButton";
             this.INCancelButton.UseVisualStyleBackColor = true;
             this.INCancelButton.Click += new System.EventHandler(this.INCancelButton_Click);
-            // 
-            // tableLayoutPanel3
-            // 
-            resources.ApplyResources(this.tableLayoutPanel3, "tableLayoutPanel3");
-            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel3.Controls.Add(this.INTextBox, 1, 0);
-            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             // 
             // label1
             // 
@@ -93,22 +66,19 @@
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.INCancelButton;
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.INNewButton);
+            this.Controls.Add(this.INTextBox);
+            this.Controls.Add(this.INCancelButton);
             this.Name = "InputName";
             this.Shown += new System.EventHandler(this.InputNameShown);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.tableLayoutPanel3.ResumeLayout(false);
-            this.tableLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Button INNewButton;
         private System.Windows.Forms.Button INCancelButton;
         private System.Windows.Forms.Label label1;
