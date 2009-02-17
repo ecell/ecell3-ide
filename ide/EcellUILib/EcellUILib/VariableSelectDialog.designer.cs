b5800aaac4a30cca8eaac0e82120243597c1970d
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2009-02-17 03:24:42 +0000

    英語のほうでEffectorに変わっていなかった。
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@2584 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ide/EcellUILib/EcellUILib/VariableSelectDialog.designer.cs
+++ ide/EcellUILib/EcellUILib/VariableSelectDialog.designer.cs
@@ -41,71 +41,47 @@
             // 
             // selectTree
             // 
-            this.selectTree.AccessibleDescription = null;
-            this.selectTree.AccessibleName = null;
             resources.ApplyResources(this.selectTree, "selectTree");
-            this.selectTree.BackgroundImage = null;
-            this.selectTree.Font = null;
             this.selectTree.Name = "selectTree";
             this.selectTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.SelectTreeDoubleClick);
             // 
             // VSProductButton
             // 
-            this.VSProductButton.AccessibleDescription = null;
-            this.VSProductButton.AccessibleName = null;
             resources.ApplyResources(this.VSProductButton, "VSProductButton");
-            this.VSProductButton.BackgroundImage = null;
-            this.VSProductButton.Font = null;
             this.VSProductButton.Name = "VSProductButton";
             this.VSProductButton.UseVisualStyleBackColor = true;
             this.VSProductButton.Click += new System.EventHandler(this.ProductButtonClick);
             // 
             // VSCloseButton
             // 
-            this.VSCloseButton.AccessibleDescription = null;
-            this.VSCloseButton.AccessibleName = null;
             resources.ApplyResources(this.VSCloseButton, "VSCloseButton");
-            this.VSCloseButton.BackgroundImage = null;
             this.VSCloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.VSCloseButton.Font = null;
             this.VSCloseButton.Name = "VSCloseButton";
             this.VSCloseButton.UseVisualStyleBackColor = true;
             // 
             // VSSourceButton
             // 
-            this.VSSourceButton.AccessibleDescription = null;
-            this.VSSourceButton.AccessibleName = null;
             resources.ApplyResources(this.VSSourceButton, "VSSourceButton");
-            this.VSSourceButton.BackgroundImage = null;
-            this.VSSourceButton.Font = null;
             this.VSSourceButton.Name = "VSSourceButton";
             this.VSSourceButton.UseVisualStyleBackColor = true;
             this.VSSourceButton.Click += new System.EventHandler(this.SourceButtonClick);
             // 
             // VSConstantButton
             // 
-            this.VSConstantButton.AccessibleDescription = null;
-            this.VSConstantButton.AccessibleName = null;
             resources.ApplyResources(this.VSConstantButton, "VSConstantButton");
-            this.VSConstantButton.BackgroundImage = null;
-            this.VSConstantButton.Font = null;
             this.VSConstantButton.Name = "VSConstantButton";
             this.VSConstantButton.UseVisualStyleBackColor = true;
             this.VSConstantButton.Click += new System.EventHandler(this.ConstantButtonClick);
             // 
             // VariableSelectDialog
             // 
-            this.AccessibleDescription = null;
-            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = null;
             this.Controls.Add(this.VSProductButton);
             this.Controls.Add(this.selectTree);
             this.Controls.Add(this.VSSourceButton);
             this.Controls.Add(this.VSCloseButton);
             this.Controls.Add(this.VSConstantButton);
-            this.Font = null;
             this.Name = "VariableSelectDialog";
             this.ResumeLayout(false);
 
