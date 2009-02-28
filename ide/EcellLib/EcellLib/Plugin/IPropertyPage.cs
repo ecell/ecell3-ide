154abfcecda5ec9112ed4b558e0c55b321e162ef
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2009-02-28 22:05:04 +0000

    設定ダイアログ用のインターフェース
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@2613 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/EcellLib/EcellLib/Plugin/IPropertyPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecell.Plugin
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IPropertyPage
+    {
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// ApplyChange
+        /// </summary>
+        void ApplyChange();
+
+        /// <summary>
+        /// PropertyDialogClosing
+        /// </summary>
+        void PropertyDialogClosing();
+    }
+}
