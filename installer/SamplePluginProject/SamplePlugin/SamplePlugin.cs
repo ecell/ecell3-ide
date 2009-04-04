cefd744f201cfd1b785695fe82bb33fc0941e196
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2009-04-04 07:15:45 +0000

    3.2対応のため変更
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@2666 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ installer/SamplePluginProject/SamplePlugin/SamplePlugin.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ecell;
+using Ecell.Plugin;
+using Ecell.Objects;
+
+namespace Ecell.IDE.Plugins.SamplePlugin
+{
+    public class SamplePlugin : PluginBase
+    {
+    }
+}
