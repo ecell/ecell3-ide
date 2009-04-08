400bd0149727fd6429e2bffca171deb51d058256
sachiboo <sachiboo@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2009-04-08 02:24:48 +0000

    globusのジョブ管理部分を作成。
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@2694 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ide/EcellLib/EcellLib/Job/GlobusJob.cs
+++ ide/EcellLib/EcellLib/Job/GlobusJob.cs
@@ -32,10 +32,14 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 
+using System.Threading;
 using System.Diagnostics;
 
 namespace Ecell.Job
 {
+    /// <summary>
+    /// Job class for globus.
+    /// </summary>
     public class GlobusJob : Job
     {
         public static string SERVER_NAME = "servername";
@@ -62,10 +66,17 @@ namespace Ecell.Job
             m_process.StartInfo.RedirectStandardInput = true;
             m_process.StartInfo.RedirectStandardOutput = true;
 
+            m_process.OutputDataReceived += new DataReceivedEventHandler(m_process_OutputDataReceived);
+            m_process.ErrorDataReceived += new DataReceivedEventHandler(m_process_ErrorDataReceived);
 
+            m_process.Start();
+            m_process.BeginErrorReadLine();
+            m_process.BeginOutputReadLine();
         }
         #endregion
-
+        /// <summary>
+        /// Retry this job.
+        /// </summary>
         public override void retry()
         {
             this.stop();
@@ -73,130 +84,298 @@ namespace Ecell.Job
             this.run();
         }
 
+        /// <summary>
+        /// Run this job.
+        /// </summary>
         public override void run()
         {
             string cmd = "";
             string argument = "";
 
-            // not implements
-            // 初期化
-            // grid-proxy-init
-            cmd = "grid-proxy-init";
+            try
+            {
+                this.Status = JobStatus.RUNNING;
 
-            // 実行
-            // cog-job-submit -e $script -args $ROOT/$JobID/$jobfile -p $provider -s $server
-            cmd = "cog-job-submit";
-            argument = " -e " + Param[GlobusJob.SCRIPT_NAME].ToString() + " -args "
-            + Param[GlobusJob.TOPDIR_NAME].ToString() + "/" + this.Machine + "/"
-            + this.JobID + "/" + ScriptFile
-            + " -p " + Param[GlobusJob.PROVIDER_NAME]
-            + " -s " + Param[GlobusJob.SERVER_NAME];
+                // 初期化
+                // grid-proxy-init
+                cmd = "grid-proxy-init";
+                m_process.StandardInput.WriteLine(cmd);
+                m_process.StandardInput.Flush();
+                m_process.StandardInput.WriteLine(Param[GlobusJob.PASSWORD].ToString());
+                m_process.StandardInput.Flush();
+
+                // 実行
+                // cog-job-submit -e $script -args $ROOT/$JobID/$jobfile -p $provider -s $server
+                cmd = "cog-job-submit";
+                argument = " -e " + Param[GlobusJob.SCRIPT_NAME].ToString() + " -args "
+                + Param[GlobusJob.TOPDIR_NAME].ToString() + "/" + this.Machine + "/"
+                + this.JobID + "/" + ScriptFile
+                + " -p " + Param[GlobusJob.PROVIDER_NAME]
+                + " -s " + Param[GlobusJob.SERVER_NAME];
+                m_process.StandardInput.WriteLine(cmd + argument);
+                m_process.StandardInput.Flush();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    if (m_process != null)
+                        m_process.Kill();                   
+                }
+                catch (Exception)
+                {
+                }
+                this.Status = JobStatus.ERROR;
+                m_process = null;
+            }
         }
 
+        /// <summary>
+        /// Stop this job.
+        /// </summary>
         public override void stop()
         {
-            // not implements
+            if (this.Status != JobStatus.RUNNING && this.Status != JobStatus.QUEUED &&
+                this.Status != JobStatus.NONE)
+                return;
+            if (m_process != null)
+            {
+                Status = JobStatus.STOPPED;
+                try
+                {
+                    m_process.Kill();
+                }
+                catch (Exception)
+                {
+                }
+                this.StdErr = "stop ...";
+                m_process = null;
+            }
+            if (Status == JobStatus.QUEUED || Status == JobStatus.RUNNING ||
+                Status == JobStatus.NONE)
+            {
+                Status = JobStatus.STOPPED;
+            }            
         }
 
         public override void Update()
         {
-            // not implements
         }
 
+        /// <summary>
+        /// Get the string of standard output.
+        /// </summary>
+        /// <returns>The string of standart output.</returns>
         public override string GetStdOut()
         {
-            // not implements
-            return base.GetStdOut();
+            return this.StdErr;
         }
 
+        /// <summary>
+        /// Get the string of standard error.
+        /// </summary>
+        /// <returns>The string of standard error.</returns>
         public override string GetStdErr()
         {
-            // not implements
-            return base.GetStdErr();
+            return this.StdErr;
         }
 
+        /// <summary>
+        /// Pprepare the execution of this job.
+        /// </summary>
         public override void PrepareProcess()
         {
             string cmd = "";
             string argument = "";
 
-            // not implements
-            // 初期化
-            cmd = "grid-proxy-init";
+            try
+            {
+                // not implements
+                // 初期化
+                cmd = "grid-proxy-init";
+                m_process.StandardInput.WriteLine(cmd);
+                m_process.StandardInput.Flush();
 
-            // 実行ディレクトリを作成
-            // cog-job-submit -e /bin/mkdir -args $ROOT/$JobID -p $Provider -s $Server           
-            cmd = "cog-job-submit";
-            argument = "-e /bin/mkdir -args " + Param[GlobusJob.TOPDIR_NAME].ToString()
-                + "/" + this.Machine + "/" + this.JobID 
-                + " -p " + Param[GlobusJob.PROVIDER_NAME].ToString()
-                + " -s " + Param[GlobusJob.SERVER_NAME];
-            
-            string dFileName = JobID + ".py";
-            File.Copy(Argument, dFileName);
-            // grid-ftpでサーバにスクリプトを持っていく
-            // cog-file-transfer -s file://tmp/$jobfile -d gsiftp://$Server/$ROOT/$JobID
-            cmd = "cog-file-transfer";
-            argument = " -s file://tmp/" + dFileName + " -d gsiftp://"
-            + Param[GlobusJob.SERVER_NAME].ToString() + "/"
-            + this.Machine + "/" + this.JobID + "/";
+                // 実行ディレクトリを作成
+                // cog-job-submit -e /bin/mkdir -args $ROOT/$JobID -p $Provider -s $Server           
+                cmd = "cog-job-submit";
+                argument = " -e /bin/mkdir -args " + Param[GlobusJob.TOPDIR_NAME].ToString()
+                    + "/" + this.Machine + "/" + this.JobID
+                    + " -p " + Param[GlobusJob.PROVIDER_NAME].ToString()
+                    + " -s " + Param[GlobusJob.SERVER_NAME];
+                m_process.StandardInput.WriteLine(cmd + argument);
+                m_process.StandardInput.Flush();
 
-            ScriptFile = dFileName;
-            base.PrepareProcess();
+                string dFileName = JobID + ".py";
+                File.Copy(Argument, dFileName);
+                // grid-ftpでサーバにスクリプトを持っていく
+                // cog-file-transfer -s file://tmp/$jobfile -d gsiftp://$Server/$ROOT/$JobID
+                cmd = "cog-file-transfer";
+                argument = " -s file://tmp/" + dFileName + " -d gsiftp://"
+                + Param[GlobusJob.SERVER_NAME].ToString() + "/"
+                + this.Machine + "/" + this.JobID + "/";
+                m_process.StandardInput.WriteLine(cmd + argument);
+                m_process.StandardInput.Flush();
+
+                ScriptFile = dFileName;
+                base.PrepareProcess();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    if (m_process == null)
+                        m_process.Kill();
+                }
+                catch (Exception)
+                {
+                }
+                this.Status = JobStatus.ERROR;
+                m_process = null;
+            }
         }
 
+        /// <summary>
+        /// Get log data from server node.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public override Dictionary<double, double> GetLogData(string key)
         {
-            string cmd = "";
-            string argument = "";
             Dictionary<double, double> result = new Dictionary<double, double>();
-            if (key == null)
-                return result;
-
-            string fileName = key.Replace("/", "_");
-            fileName = fileName + ".csv";
-
-            // not implements
-            // 初期化
-            // grid-proxy-init
-            cmd = "grid-proxy-init";
-
-            // grid-ftpでログをサーバから持ってくる
-            // cog-file-transfer -s gsiftp://$Server/$ROOT/$JobID/$logfile -d $ROOT/$JobID
-            cmd = "cog-file-transfer";
-            argument = " -d file://tmp/tmp.log -d gsiftp://"
-            + Param[GlobusJob.SERVER_NAME].ToString() + "/"
-            + this.Machine + "/" + this.JobID + "/" + fileName;
-
-          
-            // Tempに移動
-            File.Move("tmp.log", JobDirectory + "/" + fileName);
-            //File.Move($logfile, $tmpdir/$logfile)
-            // ログの読み込み
-            //
-            StreamReader hReader = new StreamReader(JobDirectory + "/" + fileName, Encoding.ASCII);
-            char splitter = '\t';
-
-            while (!hReader.EndOfStream)
+            try
             {
-                string line = hReader.ReadLine();
-                if (line.StartsWith("#")) continue;
-                string[] ele = line.Split(new char[] { splitter });
-                if (ele.Length >= 2)
-                {
-                    double time = Convert.ToDouble(ele[0]);
-                    double value = Convert.ToDouble(ele[1]);
-                    result.Add(time, value);
-                }
-            }
-            hReader.Close();
+                if (this.Status == JobStatus.ERROR)
+                    return result;
+                m_process = new Process();
+                m_process.StartInfo.FileName = "cmd.exe";
+                m_process.StartInfo.UseShellExecute = false;
+                m_process.StartInfo.CreateNoWindow = false;
+                m_process.StartInfo.RedirectStandardError = true;
+                m_process.StartInfo.RedirectStandardInput = true;
+                m_process.StartInfo.RedirectStandardOutput = true;
 
+                m_process.OutputDataReceived += new DataReceivedEventHandler(m_process_OutputDataReceived);
+                m_process.ErrorDataReceived += new DataReceivedEventHandler(m_process_ErrorDataReceived);
+
+                m_process.Start();
+                m_process.BeginErrorReadLine();
+                m_process.BeginOutputReadLine();
+
+                string cmd = "";
+                string argument = "";
+                if (key == null)
+                    return result;
+
+                string fileName = key.Replace("/", "_");
+                fileName = fileName + ".csv";
+
+                // 初期化
+                // grid-proxy-init
+                cmd = "grid-proxy-init";
+                m_process.StandardInput.WriteLine(cmd);
+                m_process.StandardInput.Flush();
+
+                // grid-ftpでログをサーバから持ってくる
+                // cog-file-transfer -s gsiftp://$Server/$ROOT/$JobID/$logfile -d $ROOT/$JobID
+                cmd = "cog-file-transfer";
+                argument = " -d file://tmp/tmp.log -d gsiftp://"
+                + Param[GlobusJob.SERVER_NAME].ToString() + "/"
+                + this.Machine + "/" + this.JobID + "/" + fileName;
+                m_process.StandardInput.WriteLine(cmd);
+                m_process.StandardInput.Flush();
+
+                while (this.Status != JobStatus.ERROR && !File.Exists(fileName))
+                {
+                    Thread.Sleep(30 * 1000);
+                }
+
+                // Tempに移動
+                File.Move("tmp.log", JobDirectory + "/" + fileName);
+                //File.Move($logfile, $tmpdir/$logfile)
+                // ログの読み込み
+                //
+                StreamReader hReader = new StreamReader(JobDirectory + "/" + fileName, Encoding.ASCII);
+                char splitter = '\t';
+
+                while (!hReader.EndOfStream)
+                {
+                    string line = hReader.ReadLine();
+                    if (line.StartsWith("#")) continue;
+                    string[] ele = line.Split(new char[] { splitter });
+                    if (ele.Length >= 2)
+                    {
+                        double time = Convert.ToDouble(ele[0]);
+                        double value = Convert.ToDouble(ele[1]);
+                        result.Add(time, value);
+                    }
+                }
+                hReader.Close();
+            }
+            catch (Exception)
+            {
+                if (m_process != null)
+                    m_process.Kill();
+                m_process = null;
+            }
             return result;
         }
 
+        /// <summary>
+        /// Get default script.
+        /// </summary>
+        /// <returns></returns>
         static public string GetDefaultScript()
         {
             return "";
         }
+
+        #region Events
+        private void m_process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.StdErr += e.Data + "\n";
+            if (this.Status == JobStatus.RUNNING && e.Data.Contains("Job Completed"))
+            {
+                this.Status = JobStatus.FINISHED;
+                if (m_process != null)
+                {
+                    m_process.Kill();
+                    m_process = null;
+                }
+            }
+            else if (this.Status == JobStatus.RUNNING && e.Data.Contains("failed"))
+            {
+                this.Status = JobStatus.ERROR;
+                if (m_process != null)
+                {
+                    m_process.Kill();
+                    m_process = null;
+                }
+            }
+        }
+
+        private void m_process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.StdErr += e.Data + "\n";
+            if (this.Status == JobStatus.RUNNING && e.Data.Contains("Job Completed"))
+            {
+                this.Status = JobStatus.FINISHED;
+                if (m_process != null)
+                {
+                    m_process.Kill();
+                    m_process = null;
+                }
+            }
+            else if (this.Status == JobStatus.RUNNING && e.Data.Contains("failed"))
+            {
+                this.Status = JobStatus.ERROR;
+                if (m_process != null)
+                {
+                    m_process.Kill();
+                    m_process = null;
+                }
+            }
+        }
+        #endregion
     }
 }
