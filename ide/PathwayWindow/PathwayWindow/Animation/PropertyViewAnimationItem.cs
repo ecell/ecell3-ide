2b1c8832cd427ee85c8f2ace7b5a171638405bac
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2009-06-13 02:27:06 +0000

    コミット漏れファイル
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@2960 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

---
+++ ide/PathwayWindow/PathwayWindow/Animation/PropertyViewAnimationItem.cs
@@ -0,0 +1,189 @@
+﻿//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//        This file is part of E-Cell Environment Application package
+//
+//                Copyright (C) 1996-2006 Keio University
+//
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//
+// E-Cell is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// E-Cell is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with E-Cell -- see the file COPYING.
+// If not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+//END_HEADER
+//
+// written by Chihiro Okada <c_okada@cbo.mss.co.jp>,
+// MITSUBISHI SPACE SOFTWARE CO.,LTD.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ecell.IDE.Plugins.PathwayWindow.Nodes;
+using System.Drawing;
+
+namespace Ecell.IDE.Plugins.PathwayWindow.Animation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PropertyViewAnimationItem : AnimationItemBase
+    {
+        #region Fields
+        private System.Windows.Forms.GroupBox variableBox;
+        private Ecell.IDE.Plugins.PathwayWindow.UIComponent.PropertyComboboxItem propertyCombobox;
+        private System.Windows.Forms.Label edgeLabel;
+        private Ecell.IDE.Plugins.PathwayWindow.UIComponent.PropertyBrushItem propertyBrush;
+        private string _property;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        public PropertyViewAnimationItem()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="control"></param>
+        public PropertyViewAnimationItem(AnimationControl control)
+            : base(control)
+        {
+            InitializeComponent();
+            SetPropertyItems();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SetPropertyItems()
+        {
+            this.propertyCombobox.ComboBox.Text = "MolarConc";
+            this.propertyCombobox.ComboBox.Items.Add("MolarConc");
+            this.propertyCombobox.ComboBox.Items.Add("NumverConc");
+            this.propertyCombobox.ComboBox.Items.Add("Value");
+            this.propertyBrush.Brush = Brushes.Blue;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PropertyViewAnimationItem));
+            this.variableBox = new System.Windows.Forms.GroupBox();
+            this.edgeLabel = new System.Windows.Forms.Label();
+            this.propertyCombobox = new Ecell.IDE.Plugins.PathwayWindow.UIComponent.PropertyComboboxItem();
+            this.propertyBrush = new Ecell.IDE.Plugins.PathwayWindow.UIComponent.PropertyBrushItem();
+            this.variableBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // variableBox
+            // 
+            resources.ApplyResources(this.variableBox, "variableBox");
+            this.variableBox.Controls.Add(this.edgeLabel);
+            this.variableBox.Controls.Add(this.propertyCombobox);
+            this.variableBox.Controls.Add(this.propertyBrush);
+            this.variableBox.Name = "variableBox";
+            this.variableBox.TabStop = false;
+            // 
+            // edgeLabel
+            // 
+            this.edgeLabel.AutoEllipsis = true;
+            this.edgeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            resources.ApplyResources(this.edgeLabel, "edgeLabel");
+            this.edgeLabel.Name = "edgeLabel";
+            // 
+            // propertyCombobox
+            // 
+            resources.ApplyResources(this.propertyCombobox, "propertyCombobox");
+            this.propertyCombobox.Name = "propertyCombobox";
+            // 
+            // propertyBrush
+            // 
+            resources.ApplyResources(this.propertyBrush, "propertyBrush");
+            this.propertyBrush.Name = "propertyBrush";
+            // 
+            // PropertyViewAnimationItem
+            // 
+            this.Controls.Add(this.variableBox);
+            this.Name = "PropertyViewAnimationItem";
+            resources.ApplyResources(this, "$this");
+            this.variableBox.ResumeLayout(false);
+            this.variableBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        #region Inherited methods
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void ApplyChange()
+        {
+            base.ApplyChange();
+            _property = this.propertyCombobox.ComboBox.Text;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void SetProperty()
+        {
+            base.SetProperty();
+
+            foreach (PPathwayVariable variable in _variables)
+            {
+                variable.PPropertyText.Visible = true;
+                variable.PPropertyText.TextBrush = propertyBrush.Brush;
+                string fullPN = variable.EcellObject.FullID + ":" + _property;
+                variable.PPropertyText.Text = this.GetTextValue(fullPN);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void UpdateProperty()
+        {
+            base.UpdateProperty();
+            foreach (PPathwayVariable variable in _variables)
+            {
+                string fullPN = variable.EcellObject.FullID + ":" + _property;
+                variable.PPropertyText.Text = this.GetTextValue(fullPN);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void ResetProperty()
+        {
+            base.ResetProperty();
+            foreach (PPathwayVariable variable in _variables)
+            {
+                variable.PPropertyText.Visible = false;
+                variable.PPropertyText.Text = "";
+            }
+        }
+        #endregion
+
+    }
+}
