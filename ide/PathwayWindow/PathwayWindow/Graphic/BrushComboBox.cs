65948bf741759915ca11dacfbc6e6bea6d107370
okada <okada@fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b>
2009-07-14 04:27:17 +0000

    ブラシ設定オブジェクトの作り直し
    git-svn-id: file:///home/moriyoshi/chaperone.e-cell.org/var/opt/subversion/ecell-ide/trunk@3116 fe1fbaf7-ad0a-4b3d-a902-8cb9c273ac6b

--- ide/PathwayWindow/PathwayWindow/Graphic/BrushComboBox.cs
+++ ide/PathwayWindow/PathwayWindow/Graphic/BrushComboBox.cs
@@ -107,6 +107,7 @@ namespace Ecell.IDE.Plugins.PathwayWindow.Graphic
         public BrushComboBox()
         {
             InitializeComponent();
+            this.comboBoxBrush.ImageList = BrushManager.GetBrushImageList();
         }
 
         /// <summary>
@@ -114,9 +115,8 @@ namespace Ecell.IDE.Plugins.PathwayWindow.Graphic
         /// </summary>
         /// <param name="brush"></param>
         public BrushComboBox(Brush brush)
+            :this()
         {
-            InitializeComponent();
-            this.comboBoxBrush.ImageList = BrushManager.GetBrushImageList();
             this.Brush = brush;
         }
 
@@ -125,155 +125,16 @@ namespace Ecell.IDE.Plugins.PathwayWindow.Graphic
             this.components = new System.ComponentModel.Container();
             this.comboBoxBrush = new Ecell.IDE.Plugins.PathwayWindow.Graphic.ImageComboBox();
             this.SuspendLayout();
+
             // 
-            // comboBoxBrush
+            // m_comboBoxBrush
             // 
             this.comboBoxBrush.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.comboBoxBrush.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.comboBoxBrush.Dock = System.Windows.Forms.DockStyle.Fill;
             this.comboBoxBrush.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
             this.comboBoxBrush.FormattingEnabled = true;
-            this.comboBoxBrush.Items.AddRange(new object[] {
-            "AliceBlue",
-            "AntiqueWhite",
-            "Aqua",
-            "Aquamarine",
-            "Azure",
-            "Beige",
-            "Bisque",
-            "Black",
-            "BlanchedAlmond",
-            "Blue",
-            "BlueViolet",
-            "Brown",
-            "BurlyWood",
-            "CadetBlue",
-            "Chartreuse",
-            "Chocolate",
-            "Coral",
-            "CornflowerBlue",
-            "Cornsilk",
-            "Crimson",
-            "Cyan",
-            "DarkBlue",
-            "DarkCyan",
-            "DarkGoldenrod",
-            "DarkGray",
-            "DarkGreen",
-            "DarkKhaki",
-            "DarkMagenta",
-            "DarkOliveGreen",
-            "DarkOrange",
-            "DarkOrchid",
-            "DarkRed",
-            "DarkSalmon",
-            "DarkSeaGreen",
-            "DarkSlateBlue",
-            "DarkSlateGray",
-            "DarkTurquoise",
-            "DarkViolet",
-            "DeepPink",
-            "DeepSkyBlue",
-            "DimGray",
-            "DodgerBlue",
-            "Firebrick",
-            "FloralWhite",
-            "ForestGreen",
-            "Fuchsia",
-            "Gainsboro",
-            "GhostWhite",
-            "Gold",
-            "Goldenrod",
-            "Gray",
-            "Green",
-            "GreenYellow",
-            "Honeydew",
-            "HotPink",
-            "IndianRed",
-            "Indigo",
-            "Ivory",
-            "Khaki",
-            "Lavender",
-            "LavenderBlush",
-            "LawnGreen",
-            "LemonChiffon",
-            "LightBlue",
-            "LightCoral",
-            "LightCyan",
-            "LightGoldenrodYellow",
-            "LightGray",
-            "LightGreen",
-            "LightPink",
-            "LightSalmon",
-            "LightSeaGreen",
-            "LightSkyBlue",
-            "LightSlateGray",
-            "LightSteelBlue",
-            "LightYellow",
-            "Lime",
-            "LimeGreen",
-            "Linen",
-            "Magenta",
-            "Maroon",
-            "MediumAquamarine",
-            "MediumBlue",
-            "MediumOrchid",
-            "MediumPurple",
-            "MediumSeaGreen",
-            "MediumSlateBlue",
-            "MediumSpringGreen",
-            "MediumTurquoise",
-            "MediumVioletRed",
-            "MidnightBlue",
-            "MintCream",
-            "MistyRose",
-            "Moccasin",
-            "NavajoWhite",
-            "Navy",
-            "OldLace",
-            "Olive",
-            "OliveDrab",
-            "Orange",
-            "OrangeRed",
-            "Orchid",
-            "PaleGoldenrod",
-            "PaleGreen",
-            "PaleTurquoise",
-            "PaleVioletRed",
-            "PapayaWhip",
-            "PeachPuff",
-            "Peru",
-            "Pink",
-            "Plum",
-            "PowderBlue",
-            "Purple",
-            "Red",
-            "RosyBrown",
-            "RoyalBlue",
-            "SaddleBrown",
-            "Salmon",
-            "SandyBrown",
-            "SeaGreen",
-            "SeaShell",
-            "Sienna",
-            "Silver",
-            "SkyBlue",
-            "SlateBlue",
-            "SlateGray",
-            "Snow",
-            "SpringGreen",
-            "SteelBlue",
-            "Tan",
-            "Teal",
-            "Thistle",
-            "Tomato",
-            "Turquoise",
-            "Violet",
-            "Wheat",
-            "White",
-            "WhiteSmoke",
-            "Yellow",
-            "YellowGreen"});
+            this.comboBoxBrush.Items.AddRange(BrushManager.GetBrushNameList().ToArray());
             this.comboBoxBrush.Location = new System.Drawing.Point(0, 0);
             this.comboBoxBrush.MaxDropDownItems = 10;
             this.comboBoxBrush.Name = "comboBoxBrush";
